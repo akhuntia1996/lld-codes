fd99b0cfe31eb10b8442787182376faf3469576c
Abhishek Khuntia <36916011+akhuntia1996@users.noreply.github.com>
2022-10-05 22:00:08 +0530

    VendingMachine - Phase 1

---
+++ VendingMachine/states/IdleState.java
@@ -0,0 +1,7 @@
+public class IdleState extends State {
+
+    public State insertMoney(int amount) {
+        
+        return null;
+    }
+}
---
+++ VendingMachine/states/State.java
@@ -0,0 +1,18 @@
+public abstract class State {
+
+    public State insertMoney() {
+        System.out.println("Not allowed !!");
+        return null;
+    }
+
+    public State showProducts() {
+        System.out.println("Not allowed !!");
+        return null;
+    }
+
+    public State checkBalance() {
+        System.out.println("Not allowed !!");
+        return null;
+    }
+
+}
